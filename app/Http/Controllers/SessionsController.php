280e8d2035d6685c14ba52f38a91c1d15b874273
Yanhk <705177961@qq.com>
2019-02-16 14:50:27 +0800

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -30,4 +30,12 @@ class SessionsController extends Controller
         }
         return ;
     }
+
+    //退出登录
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
+    }
 }
